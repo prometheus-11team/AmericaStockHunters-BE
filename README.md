4fead0e39cee334a5a239d2cdd449b2a2230c1c7
장지수 <wltnslagg@naver.com>
2025-07-25 22:39:45 +0900

    [FEAT] 프론트 첫 페이지 POST 메서드 연결

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # AmericaStockHunters-BE
-프로메테우스 7기 - 프메개미: 미국주식 자동매매 프로젝트 웹 백엔드
+>프로메테우스 7기 - 프메개미: 미국주식 자동매매 프로젝트 웹 백엔드 입니다.
+---
+## Quick Start
+``` bash
+conda create {your-env} python=3.11 -y
+conda activate {your-env}
+
+pip install fastapi
+pip install "uvicorn[standard]"
+
+git clone git@github.com:prometheus-11team/AmericaStockHunters-BE.git
+cd AmericaStockHunters-BE
+
+uvicorn main:app --reload
+```
+---
+### Check with UI
+``` bash
+cd ..
+git clone git@github.com:prometheus-11team/AmericaStockHunters-FE.git
+cd AmericaStockHunters-FE
+
+# make sure the installation of Node.js (including npm)
+npm install
+npm start
+```
--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS middleware to allow requests from the frontend (개발 중엔 모든 origin 허용)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 origin 허용 (개발 중, 배포 시에는 특정 origin으로 제한이 권장됨)
+    allow_credentials=True, # 쿠키와 같은 인증 정보를 허용
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+class TradeRequest(BaseModel):
+    name: str
+    initialCapital: int
+    startDate: str
+    endDate: str
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to America Stock Hunters Backend!"}
+
+@app.post("/api/v1/start") # fastapi는 POST 방식일 때 자동으로 request body에서 데이터를 추출
+def create_user(data: TradeRequest):
+    """ 
+    Create a new user with the provided data.
+    This endpoint is used to start trading with the given parameters.
+    """
+    print("Received trading request:", data)
+    # 실제 트레이딩 로직을 추가 필요
+    
+    return {"message": f"{data.name} 의 트레이딩이 시작됨!"}
